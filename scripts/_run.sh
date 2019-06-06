50de5f82cee060fb00fd1cc37ad27e0d3f0a41bc
Lewis Daly <lewisdaly@me.com>
2019-06-06 11:55:26 +0800

    fix up minor issues

--- scripts/_run.sh
+++ scripts/_run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LIB_DIR=${ROOT_DIR}/lib
 ROOT_DIR=${DIR}/..
+LIB_DIR=${ROOT_DIR}/lib
 
 source .env
 eval $(${LIB_DIR}/toml-to-env/bin/toml-to-env.js ${ROOT_DIR}/config.toml)
