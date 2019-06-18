60258029bc604b308dc6b2046bf026a1632576bc
Lewis Daly <lewisdaly@me.com>
2019-06-18 10:42:25 +0800

    working on standalone mode

--- scripts/_checkout.sh
+++ scripts/_checkout.sh
@@ -7,6 +7,12 @@ LIB_DIR=${ROOT_DIR}/lib
 source .env
 eval $(${LIB_DIR}/toml-to-env/bin/toml-to-env.js ${ROOT_DIR}/config.toml)
 
+if [ ${mode} == "ci"]; then
+  echo 'ci mode specified. Skipping checkout'
+  exit 0
+fi
+
+
 mkdir -p ${ROOT_DIR}/checked_out
 cd ${ROOT_DIR}/checked_out
 
